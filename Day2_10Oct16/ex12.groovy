77422d7900e8cbc4f583e0c8aed38bf4bfdd7bf0
alex oyedele <alex.oyedele@projected-solutions.com>
2016-10-19 13:00:50 +0100

    commitex12

---
+++ Day2_10Oct16/ex12.groovy
@@ -0,0 +1,11 @@
+//number pyramids
+
+int count = 1
+int pyrSize = 7
+int count = 0
+
+while(count <=pyrSize){
+	println String.valueOf(count)*count
+	count ++
+}
+Contact GitHub API Training Shop Blog About
