b8a1fa8eff757de8a27d56ce33f48a82f46b4b04
alex oyedele <alex.oyedele@projected-solutions.com>
2016-10-20 11:28:17 +0100

    commit2ex

---
+++ Day3_17Oct16/02ex.groovy
@@ -0,0 +1,45 @@
+//calculator program
+//input read two numbers from the user
+// then the program offers four basic operations - addition, subtraction, multiplication, division
+//output +,-,* and / 
+
+String str = ""
+double num1 = 0.0
+double num2 = 0.0
+def operatorList = ["+","-","*", "/"]
+int option = 0
+
+for(i=0;i<2;i++){
+	if(i==0){
+		print("Please enter a number: ")
+		str = System.console().readLine()
+		num1 = Double.parseDouble(str)		
+	} else{
+		print("Please enter another number: ")
+		str = System.console().readLine()
+		num2 = Double.parseDouble(str)
+		print("What would you like to do: +, -, *, / : ")
+		str = System.console().readLine()
+		option = operatorList.indexOf(str) + 1
+		println option
+		switch(option){
+			case 1: 
+				print(num1 + num2)
+				break;
+			case 2: 
+				print(num1 - num2)
+				break;
+			case 3: 
+				print(num1 * num2)
+				break;
+			case 4: 
+				print(num1 / num2)
+				break;
+			default: 
+				print("Crash, ahhhhh!")
+				break;
+
+		}
+	}
+}
+
